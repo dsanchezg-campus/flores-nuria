fce5cf9532bf714c6c19834908253e1e4d2d4c6e
mlizanoj <mlizanoj@campusdigitalfp.com>
2026-05-19 09:11:23 +0200

    Archivos de ejemplo creados

---
+++ public/payments.php
@@ -0,0 +1,10 @@
+<main class="main">
+    <?php include __DIR__ . '/header.php'; ?>
+    <section class="container">
+        <section class="panel">
+            <h2>Cobros/pagos</h2>
+            <p>Esta es una página de ejemplo para la sección de cobros/pagos.</p>
+        </section>
+        <footer class="footer">© 2026 Flores Nuria · Empleados</footer>
+    </section>
+</main>
---
+++ public/schedule.php
@@ -0,0 +1,10 @@
+<main class="main">
+    <?php include __DIR__ . '/header.php'; ?>
+    <section class="container">
+        <section class="panel">
+            <h2>agenda</h2>
+            <p>Esta es una página de ejemplo para la sección de agenda.</p>
+        </section>
+        <footer class="footer">© 2026 Flores Nuria · Empleados</footer>
+    </section>
+</main>
