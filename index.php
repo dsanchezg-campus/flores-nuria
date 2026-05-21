df25f9d9c20e7c83f78f08dc134873cf19c6f543
mlizanoj <mlizanoj@campusdigitalfp.com>
2026-05-21 09:04:15 +0200

    Logo y stilos hover en botonnes eliminar y editar

--- index.php
+++ index.php
@@ -17,6 +17,7 @@ if (isset($_SESSION['msg'])) {
   <meta name="viewport" content="width=device-width,initial-scale=1">
   <title>Floristería - Panel</title>
   <link rel="stylesheet" href="css/styles.css">
+    <link rel="icon" href="img/logo_fondo_blanco.jpg">
   <script src="js/menu.js" defer></script>
 </head>
 <body>
@@ -24,6 +25,7 @@ if (isset($_SESSION['msg'])) {
     <?php
       $page = $_GET['page'] ?? 'dashboard';
       include __DIR__ . '/public/sidebar.php';
+//      Paginas permitidas para evitar fallos
       $allowedPages = ['dashboard', 'products', 'employees', 'reports', 'payments', 'schedule', 'customers', 'suppliers', 'invoices', 'deliveries', 'budgets', 'create_product', 'create_supplier', 'orders', 'create_order', 'offers'];
       if(in_array($page, $allowedPages, true)){
         include __DIR__ . '/public/' . $page . '.php';
