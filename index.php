d55211fb24c117309894e960b82092f4220b94f0
mlizanoj <mlizanoj@campusdigitalfp.com>
2026-05-27 12:41:01 +0200

    Comentario

--- index.php
+++ index.php
@@ -31,6 +31,7 @@ if (isset($_SESSION['msg'])) {
   <section class="app">
     <?php
       $page = $_GET['page'] ?? 'dashboard';
+      // __DIR__ es la ruta absoluta del directorio actual, lo que evita problemas attm:D
       include __DIR__ . '/public/sidebar.php';
 //      Paginas permitidas para evitar fallos
       $allowedPages = ['dashboard', 'products', 'employees', 'reports', 'payments', 'schedule', 'customers', 'suppliers', 'invoices', 'deliveries', 'budgets', 'create_product', 'create_supplier', 'orders', 'create_order', 'offers','create_offer', 'create_tiket', 'create_payment'];
